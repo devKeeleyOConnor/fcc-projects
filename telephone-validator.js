8ebf258f10d40df78d4cf5835d35005ff7a34f31
Keeley O'Connor <dev.keeley.oconnor@gmail.com>
2021-10-04 23:06:24 -0500

    Telephone Validator Tool upload

--- telephone-validator.js
+++ telephone-validator.js
@@ -7,43 +7,42 @@
 // Return true if the string is a valid US phone number; otherwise return false.
 
 //Allowed number formats: 
-// 555-555-5555
-// (555)555-5555
-// (555) 555-5555
-// 555 555 5555
-// 5555555555
-// 1 555 555 5555
+// 555-555-5555 || 1 555-555-5555
+// 555 555 5555 || 1 555 555 5555 (5555555555 || 15555555555)
+// (555)555-5555 || 1 (555)555-5555
+// (555)555 5555 || 1 (555)555 5555
 
 function telephoneCheck(str) {
-    str = str.replace(/\s/g, ""); //Remove any whitespace in the str.
-    let regexNum = [
-        /\d{3}-\d{3}-\d{4}/g,
-        /(\d{3})\d{3}-\d{4}/g,
-        /\d{10,11}/g,
-        /1\(\d{3}\)\d{3}-\d{4}/g,
-        /\(\d{3}\)\d{3}-\d{4}/g,
-        /\(\d{3}-\d{3}-\d{4}\)/g,
-        /\(\d{10,15}\)/g
-    ];
-    let regexLetter = /[a-z]/gi;
-
-
-// if ((regexLetter.test(str) == true) ||       //Check arguement for letters
-//     (str.length < 10) ||                     //Check arguement for correct length w/o area code
-//     (((str.length == 11) || (str.length == 14)) && (str[0] != 1)) ||
-//     ((str.length > 14))){  //Check arguement for correct length with area code & correct country code
-//         return false;
-// }else 
-    if(  (regexNum[0].test(str) == true) ||
-           (regexNum[1].test(str) == true) ||
-           (regexNum[2].test(str) == true) ||
-           (regexNum[3].test(str) == true) ||
-           (regexNum[4].test(str) == true) ||
-           (regexNum[5].test(str) == true)){
-               return true;
-           }
-return false;
+// Create an array containing regex to match for correct validation with and without country code (1).
+let regex = [
+    /^\d{10}$/g,                           // 555 555 5555
+    /^1\d{10}$/g,                          // 1 555 555 5555
+    /^\(\d{3}\)\d{7}$/g,                   // (555) 555 5555
+    /^1\(\d{3}\)\d{7}$/g,                  // 1 (555) 555 5555
+    /^\(\d{3}\)\d{3}\-\d{4}$/g,             // (555) 555 - 5555
+    /^1\(\d{3}\)\d{3}\-\d{4}$/g,            // 1 (555) 555 - 5555
+    /^\d{3}-\d{3}-\d{4}$/g,                // 555 - 555 - 5555
+    /^1\d{3}\-\d{3}\-\d{4}$/g                // 1 555 - 555 - 5555
+];
+//Remove any whitespace in the str
+    str = str.replace(/\s/g, "");       
+console.log(str);
+//Check the str against each index in the regex array for a proper match
+if ((regex[0].test(str) == true) ||
+    (regex[1].test(str) == true) ||
+    (regex[2].test(str) == true) ||
+    (regex[3].test(str) == true) ||
+    (regex[4].test(str) == true) ||
+    (regex[5].test(str) == true) ||
+    (regex[6].test(str) == true) ||
+    (regex[7].test(str) == true)){
+        return true;
+    }else{
+        return false;
+    }
 };    
-   
 
-console.log(telephoneCheck("333-333-3333"));
+console.log(telephoneCheck("5555555555"));
+console.log(telephoneCheck("(555)555-5555"));
+console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("1 555-555-5555"));
